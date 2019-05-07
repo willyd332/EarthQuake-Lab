866fbb6d62615d93a00360c542a40f4f175e7af8
WillyD <sopwill3@gmail.com>
2019-05-07 11:21:05 -0600

    finished earthquakes componnent

--- earthquakes/src/EarthquakesIndex/EarthquakesIndex.jsx
+++ earthquakes/src/EarthquakesIndex/EarthquakesIndex.jsx
@@ -1,28 +1,42 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class EarthquakesIndex extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      earthquakes: [],
+      earthquakes: []
     }
   }
 
-  createEarthquakes(){
-    
+  componentDidMount(){
+    this.getQuakes()
+  }
+  getQuakes = async () => {
+
+    const foundEarthquakes = await fetch('http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson')
+
+    const parsedEarthquakes = await foundEarthquakes.json()
+
+    const earthquakesArray = parsedEarthquakes.features;
+    console.log(earthquakesArray);
+
+    const earthquakesShow = earthquakesArray.map((currQuake) => {
+      return <li key={currQuake.properties.ids}>
+        <h1>Magnitude: {currQuake.properties.mag}</h1>
+        <h2>Location: {currQuake.properties.place}</h2>
+        <h3>Lon: {currQuake.geometry.coordinates[0]}&deg; Lat: {currQuake.geometry.coordinates[1]}&deg; Depth: {currQuake.geometry.coordinates[2]}KM</h3>
+      </li>
+    })
+
+    this.setState({earthquakes: earthquakesShow})
+
   }
 
-  render(){
-    const earthquakes = this.createEarthquakes()
-    return(
-      <div>
-        <ul>
-          {earthquakes}
-        </ul>
-      </div>
-    )
+  render() {
+    return (<div>
+      <ul>{this.state.earthquakes}</ul>
+    </div>)
   }
 }
-
 
 export default EarthquakesIndex;
